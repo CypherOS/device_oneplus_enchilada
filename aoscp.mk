eeaffec9fdb40c70f53e10febb5a5838ff35bed7
Chris <chris@cypheros.co>
2018-07-25 04:10:38 -0400

    Initial bringup for aoscp
    Change-Id: Iea88b0e654e1e12a3a5f7819108e2a66b4a44b84

--- aoscp.mk
+++ aoscp.mk
@@ -21,10 +21,10 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit from enchilada device
 $(call inherit-product, device/oneplus/enchilada/device.mk)
 
-# Inherit some common Lineage stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+# Inherit some common aoscp stuff.
+$(call inherit-product, vendor/aoscp/configs/common_full_phone.mk)
 
-PRODUCT_NAME := lineage_enchilada
+PRODUCT_NAME := aoscp_enchilada
 PRODUCT_DEVICE := enchilada
 PRODUCT_MANUFACTURER := OnePlus
 PRODUCT_BRAND := OnePlus
